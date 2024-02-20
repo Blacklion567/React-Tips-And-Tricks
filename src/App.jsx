550ab6caf94463d5ecdb94007889bb2a111b90ae
Blacklion567 <naviedaj567@gmail.com>
2024-02-20 22:21:08 +0800

    React-Tips-And-Tricks

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import ReactMistakes01 from "./BeginnerMistakesRuinedYouAsADeveloper/ReactMistakes01";
+// import ReactMistakes02 from "./BeginnerMistakesRuinedYouAsADeveloper/ReactMistakes02";
+
+import ReactMistakes03 from "./BeginnerMistakesRuinedYouAsADeveloper/ReactMistakes03";
+
+
+function App() {
+  return (
+    <>
+      {/* <ReactMistakes01 />
+      <ReactMistakes02 />*/}
+      <ReactMistakes03 />
+    </>
+  );
+}
+
+export default App;
